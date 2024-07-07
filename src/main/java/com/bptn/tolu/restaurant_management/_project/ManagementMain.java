f2eaa79029fb4c7bcb78da30945b9bdcaa9750ef
tolu <103815106+teuniced@users.noreply.github.com>
2024-07-07 16:33:34 -0400

    Initial Commit

--- src/main/java/com/bptn/tolu/restaurant_management/_project/ManagementMain.java
+++ src/main/java/com/bptn/tolu/restaurant_management/_project/ManagementMain.java
@@ -1,96 +1,111 @@
 package com.bptn.tolu.restaurant_management._project;
 
 //importing  scanner util
-import  java.util.Scanner;
 
+import java.util.Scanner;
 
 public class ManagementMain {
 
 	private Scanner scanner;
-	int userchoice;
+	private Menu menu;
+	// tutorials point ANSI color
+	String reset = "\u001B[0m";
+	String red_text = "\u001B[31m"; 
+    String green_text = "\u001B[32m";
+    String yellow_text = "\u001B[33m";
+	String blue_bg = "\u001B[44m";
+	String cyan_bg = "\u001B[46m";
+	String black_bg = " \u001B[40m";
+	String red_bg = "\u001B[41m";
+	String white_bg = "\u001B[47m";
+	String green_bg = "\u001B[42m";
+	String yellow_bg = "\u001B[43m";
+	String magenta_bg = "\u001B[45m";
 
-	//scanner input for user
+	// scanner input for user
 	public ManagementMain() {
 		scanner = new Scanner(System.in);
+		menu = new Menu();
 	}
 
-	//Main run() method
+	// Main run() method
 	public void run() {
-		System.out.println("Welcome!");
+		System.out.println(green_text + "Welcome!" + reset);
+		int userchoice;
 
 		do {
 			displayMainMenu();
 			userchoice = getUserChoice();
 			manageUserChoice(userchoice);
 		} while (userchoice != 7);
-			
-		scanner.close();
-		
-	}
-		
-		private void displayMainMenu() {
-			System.out.println("..........Main Menu..........");
-			System.out.println("1) Manage Menu");
-			System.out.println("2) Process Orders");
-			System.out.println("3) Manage Customers");
-			System.out.println("4) Manage Inventory");
-			System.out.println("5) Manage Billing");
-			System.out.println("6) Manage Recipe Book");
-			System.out.println("7) Exit");
-		}
-	
-		private int getUserChoice() {
-			System.out.print("Enter your choice: ");
-			while (!scanner.hasNextInt()) {
-				System.out.println("Kindly select a valid choice!");
-				scanner.next();
-			}
-			return scanner.nextInt();
-		}
-	
-		private void manageUserChoice(int userchoice) {
-			switch(userchoice) {
-			case 1:
-				System.out.println("Search Menu");
-				//my logic
-				break;
-							
-			case 2:
-				System.out.println("Search Orders");
-				//my logic
-				break;
-				
-				
-			case 3:
-				System.out.println("Search Customers");
-				//my logic
-				break;
-				
-			case 4:
-				System.out.println("Search Inventory");
-				//my logic
-				break;
-				
-			case 5:
-				System.out.println("Processing Billing");
-				//my logic
-				break;
-				
-			case 6:
-				System.out.println("Search for Recipes");
-				//my logic
-				break;
-				
-			case 7:
-				System.out.println("Exiting, Comeback again!");
-				//my logic
-				break;
-			default:
-				System.out.println("Kindly select a valid choice!");
-			
-			}
-		}
 
+		scanner.next();
+		scanner.close();
+	}
+
+	private void displayMainMenu() {
+		System.out.println(green_text + "..........Main Menu..........");
+		System.out.println("1) Manage Menu");
+		System.out.println("2) Process Orders");
+		System.out.println("3) Manage Customers");
+		System.out.println("4) Manage Inventory");
+		System.out.println("5) Manage Billing");
+		System.out.println("6) Manage Recipe Book");
+		System.out.println(green_text + "7) Exit" + reset);
+	}
+
+	private int getUserChoice() {
+		System.out.print("Enter your choice: \n");
+		while (!scanner.hasNextInt()) {
+			System.out.println(red_text + "Kindly select a valid choice! \n" + reset);
+			scanner.next();
+		}
+		return scanner.nextInt();
+	}
+
+	private void manageUserChoice(int userchoice) {
+		switch (userchoice) {
+		case 1:
+			System.out.println("Search Menu");
+			menu.manageMenu(scanner);
+			break;
+
+		case 2:
+			System.out.println("Search Orders");
+			// my logic
+			break;
+
+		case 3:
+			System.out.println("Search Customers");
+			// my logic
+			break;
+
+		case 4:
+			System.out.println("Search Inventory");
+			// my logic
+			break;
+
+		case 5:
+			System.out.println("Processing Billing");
+			// my logic
+			break;
+
+		case 6:
+			System.out.println("Search for Recipes");
+			// my logic
+			break;
+
+		case 7:
+			System.out.println("Exiting, Comeback again!");
+			// my logic
+			break;
+		default:
+			System.out.println(red_text + "Kindly select a valid choice! \n " + reset);
+
+		}
+	}
+	
+	
 	public static void main(String[] args) {
 		ManagementMain app = new ManagementMain();
 		app.run();
---
+++ src/main/java/com/bptn/tolu/restaurant_management/_project/Menu.java
@@ -0,0 +1,128 @@
+package com.bptn.tolu.restaurant_management._project;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+
+
+public class Menu {
+	
+	// tutorials point ANSI color
+	String reset = "\u001B[0m";
+	String red_text = "\u001B[31m"; 
+    String green_text = "\u001B[32m";
+    
+    private Map<String, List<String>> weeklyMenu ;
+    
+    
+
+	public Menu() {
+		weeklyMenu = new HashMap<>();
+	weeklyMenu.put("Sunday", List.of("Caesar Salad","Strawberry Shake","Tea", "Pop", 
+			"Fruit Salad", "Spahetti Bolognese", "Fish & Chips", "Mashed Potatoes with Chicken & Creamy Dijon Sauce", "Water" , "Lemonade"));
+	
+	weeklyMenu.put("Monday", List.of("Eggs Benedict"," Vanilla MilkShake","Tea", "Pop", 
+			"Gingerbread Pancakes", "Crispy Chicken Sandwich", "Fish & Chips", "Rad Thai","Water", "Lemonade"));
+	
+	weeklyMenu.put("Tuesday", List.of("Smoked Salmon Bagel Sandwich","Shake","Tea", "Pop", 
+			"Cobb Salad", "Spahetti Bolognese", "BBQ Chicken Pizza ", "BBQ Side Ribs with Fries", "Water","Lemonade"));
+	
+	weeklyMenu.put("Wednesday", List.of("Steamed Clams","Hot Fudge Sundae","Tea", "Pop", 
+			"Sweet Kale Salad", "Jollof Rice", "Apple Cider Braised Beef", "Gauacamole & Chips ","Water","Lemonade"));
+	
+	weeklyMenu.put("Thursday", List.of("Caesar Salad","Shake","Tea", "Pop", 
+			"Curry Fried Rice", "Steamed Vegetables", "Fish & Chips", "Kali Flatbread", "Water","Lemonade"));
+	
+	weeklyMenu.put("Friday", List.of("Cinnamon Sugar Donuts","Chocolate Cake","Tea", "Pop", 
+			"Blueberry Pancakes", "Toasted BLT", "Burrito", "Lemon Garlic Creamy Linguini","Water","Lemonade", "Sparkling Water"));
+	
+	weeklyMenu.put("Saturday", List.of("Apple Pie","Oreo MilkShake","Tea", "Pop", 
+			"Tiramisu", "Waffles and Chicken", "Carrot Soup", "Westinghouse Burger", "Fries", "Water","Lemonade"));
+}
+
+	
+	//SCANNER FOR USER INPUT
+	public void manageMenu(Scanner scanner) {
+		int userchoice;
+		
+		do {
+			System.out.println(green_text + "..........Menu Management..........");
+			System.out.println("1) View Full Week Menu");
+			System.out.println("2) View Menu For the Current Day");
+			System.out.println("3) View Special Menu");
+			System.out.println("4) Return to Main Menu");
+			System.out.println(green_text + "5) Enter your Choice:\n" + reset );
+			
+			userchoice = scanner.nextInt();
+			scanner.nextLine();
+			
+			switch (userchoice) {
+			case 1:
+				System.out.println("View Full Week Menu");
+				viewFullWeekMenu();
+				break;
+
+			case 2:
+				System.out.println("View Menu For the Current Day");
+				viewMenuForDay(scanner);
+				break;
+
+			case 3:
+				System.out.println("View Special Menu");
+				filterMenuWithE();
+				break;
+
+			case 4:
+				System.out.println("Returning to Main Menu");
+				// my logic
+				break;
+				
+			default:
+				System.out.println(red_text + "Kindly select a valid choice!" + reset);		
+		}
+	} while (userchoice != 4);
+}	
+	
+	//MENU FOR THE WEEK
+	private void viewFullWeekMenu() {
+		weeklyMenu.forEach((day, menulist) -> System.out.println("Here's " + day + "'s Menu: " + String.join(",", menulist)+ "\n"));
+		
+	}
+	
+	//MENU FOR THE DAY 
+	private void viewMenuForDay(Scanner scanner) {
+		System.out.println("Enter day of the week: ");
+		String dayoftheweek = scanner.nextLine();
+		List <String> dailymenu = weeklyMenu.get(dayoftheweek);
+		if (dailymenu!= null){
+			System.out.println(dayoftheweek + String.join(",", dailymenu) );
+		}else {
+			System.out.println("Invalid day entered");
+		}
+	}
+	
+	
+	//SPECIAL MENU
+	private void filterMenuWithE() {
+		List<String> dayswithE = weeklyMenu.keySet().stream()
+				.filter(dayoftheweek -> dayoftheweek.toLowerCase()
+				.contains("e"))
+				.collect(Collectors.toList());
+				
+		System.out.println("Special Menu for Tuesday & Wednesday");
+		dayswithE.forEach(dayoftheweek -> {
+			System.out.println(dayoftheweek + String.join(",", weeklyMenu.get(dayoftheweek)));
+		});
+	}
+}
+
+
+
+//	public static void main(String[] args) { //i didnt need a main..find out why and how that worked
+//		
+//
+//	}
